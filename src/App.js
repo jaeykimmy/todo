8df63a07fc892e513e51f279b0475eff154233f2
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-06-24 15:01:34 -0400

    task completion syncs with data in browser

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import { useState } from 'react';
 import Todo from './components/Todo';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
+import { nanoid } from 'nanoid';
 
 function App(props) {
   const [tasks, setTasks] = useState(props.tasks)
   function addTask(name) {
-    const newTask = { id: "id", name: name, completed: false };
+    const newTask = { id: "todo" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask])
   }
 
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed }
+      }
+      return task;
+    });
+    setTasks(updatedTasks)
+  }
   const taskList = tasks.map(task =>
     <Todo
       id={task.id}
       name={task.name}
       completed={task.completed}
-      key={task.id} />);
-
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted} />);
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -28,7 +40,7 @@ function App(props) {
         <FilterButton/>
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       <ul
         role="list"
